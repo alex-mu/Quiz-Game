d43206d4cac2d2c19a555764ee09ae448cef20c5
Alexander <alexndegwa46@gmail.com>
2022-02-17 21:36:21 +0300

    forms installed correctly

--- quiz.py
+++ quiz.py
@@ -1,1 +1,24 @@
-from flask import Flask, render_template, redirect, url_for
+from dataclasses import field
+from email import message
+from typing_extensions import Self
+from flask_wtf import FlaskForm as Form
+from wtforms import RadioField
+from wtforms.validators import ValidationError
+from random import randrange
+
+
+class CorrectAnswer(object):
+    def __init__(self, answer):
+        self.answer = answer
+def __call__(self, form, field):
+        message = 'Incorrect answer.'
+if field.data != Self.answer:
+            raise ValidationError(message)
+class PopQuiz(Form):
+    class Meta:
+        csrf = False
+    q1 = RadioField(
+        "The answer to question one is False.",
+        choices=[('True', 'True'), ('False', 'False')],
+        validators=[CorrectAnswer('False')]
+        )
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 class Config:
     debug = True
@@ -7,3 +7,17 @@ class Config:
 
     app = Flask(__name__)
     app.config.from_object(__name__)
+
+    @app.route('/', methods=['GET', 'POST'])    
+    def wtf_quiz(): 
+        form = PopQuiz() 
+        if form.validate_on_submit(): 
+            return redirect(url_for('passed')) 
+            return render_template('quiz.html', form=form)
+
+    @app.route('/passed')
+    def  passed(): 
+        return render_template('passed.html')
+
+    if __name__ == '__main__': 
+        app.run(debug=True)
