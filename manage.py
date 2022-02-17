191eadb11068bec514f9343a7db9e7b051fe7510
Eric-Gona <eric.gona254@gmail.com>
2022-02-17 11:42:29 +0300

    changes

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from app import create_app, render_template, request
     
     
-app = Flask(__name__)
+app = create_app(__name__)
     
     
 @app.route("/", methods=['GET', 'POST'])
